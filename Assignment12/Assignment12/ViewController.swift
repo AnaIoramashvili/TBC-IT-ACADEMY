dc4d3e911482f25353aad5af44ee139239f4786f
Ioramka <ana.ioramashvili.1@btu.edu.ge>
2024-04-06 01:40:35 +0400

    Assignment12

---
+++ Assignment12/Assignment12/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Assignment12
+//
+//  Created by Ana on 4/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Image: UIImageView!
+    
+    @IBOutlet weak var CoffeName: UILabel!
+    
+    @IBOutlet weak var Rating: UIStackView!
+    @IBOutlet weak var HeartImage: UIImageView!
+
+    @IBOutlet var Line: UIView!
+    
+    @IBOutlet weak var Description: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+}
