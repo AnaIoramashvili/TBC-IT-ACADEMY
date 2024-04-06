e86d019a6c5955d5ebf176c882e75398ce77f92e
Ioramka <ana.ioramashvili.1@btu.edu.ge>
2024-04-06 05:11:09 +0400

    Assignment12

--- Assignment12/Assignment12/ViewController.swift
+++ Assignment12/Assignment12/ViewController.swift
@@ -8,22 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var Image: UIImageView!
     
-    @IBOutlet weak var CoffeName: UILabel!
-    
-    @IBOutlet weak var Rating: UIStackView!
-    @IBOutlet weak var HeartImage: UIImageView!
-
-    @IBOutlet var Line: UIView!
-    
-    @IBOutlet weak var Description: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var ratingStackView: UIStackView!
+    @IBOutlet weak var heartImageView: UIImageView!
+    @IBOutlet weak var lineView: UIView!
+    @IBOutlet weak var coffeeNameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var sizeLabel: UILabel!
+    @IBOutlet weak var sizeStackView: UIStackView!
+    @IBOutlet weak var priceAmountLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var buyButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-    }
+        imageView.layer.cornerRadius = 18
 
+        
+        struct Coffee {
+            var productName: String
+            var price: Double
+            var productSize: String
+        }
+
+            
+        let coffee = Coffee(productName: "კაპუჩინო", price: 4.53, productSize: "საშუალო")
+
+        
+        coffeeNameLabel.text = coffee.productName
+        priceAmountLabel.text = "₾ \(coffee.price)"
+
+    }
 }
+        
