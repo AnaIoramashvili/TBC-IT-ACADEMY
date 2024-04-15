df4855546c764102989f8606645df68c9307989d
Ioramka <ana.ioramashvili.1@btu.edu.ge>
2024-04-15 19:43:12 +0400

    Updated Assignment 16

---
+++ Assignment16/Assignment16/Scenes/CollectionViewCell/CustomCell.swift
@@ -0,0 +1,12 @@
+//
+//  CustomCell.swift
+//  Assignment16
+//
+//  Created by Ana on 4/15/24.
+//
+
+import UIKit
+
+class CustomCell: UICollectionViewCell {
+    
+}
---
+++ Assignment16/Assignment16/Scenes/CollectionViewCell/PostInfoModel.swift
@@ -0,0 +1,8 @@
+//
+//  PostInfoModel.swift
+//  Assignment16
+//
+//  Created by Ana on 4/15/24.
+//
+
+import Foundation
